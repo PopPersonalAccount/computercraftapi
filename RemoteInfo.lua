d6c6fae3270057d17b42edc9886248b6ee2e988a
Pop <95447025+PopPersonalAccount@users.noreply.github.com>
2022-07-16 02:14:28 +0200

    Update

--- RemoteInfo.lua
+++ RemoteInfo.lua
@@ -1,7 +1,7 @@
 local left = peripheral.wrap("left")
 local right = peripheral.wrap("right")
 local power = "placeholder"
-local me = '{"items":'
+local me = ''
 local content = ''
 function check()
     local maxEnergy = left.getMaxEnergyStored("unknown")
@@ -12,11 +12,11 @@ function check()
     else 
         power = "Offline or very low"
     end
-    power = '{"power": ' ..'"'.. power .. '"},'
+    power = '["'..power..'"]'
 end
 function system()
     for o,l in pairs(right.getAvailableItems()) do
-        me = me..'"'..'{"'..l["name"]..':'..'"'..l["amount"]..'"'..'},'
+        me = me..'["'..l["name"]..'",'..'"'..l["qty"]..'"'..']'..','
     end
 end
 
@@ -24,12 +24,13 @@ function send(content)
 url = "https://poppersonalaccount-computercraftapi-5g5jq6446fpwv-5000.githubpreview.dev/"
 local post, err = http.post(url, content)
 content = ''
-me = '{"items":'
+me = ''
 if not post then
     printError(err)
     end
 end
 system()
 check()
-content = content..power..me..'}'
+content = content..'{"info":['..power..','..me
+
 send(content)
